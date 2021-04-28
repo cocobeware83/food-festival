5ff086b1a3da92cdc88ba3133575b56dcb0f7f92
ccneel <corycneel@gmail.com>
2021-04-28 15:50:01 -0500

    merging to main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,6 @@
 require('bootstrap');
-<<<<<<< HEAD
 const img1 = require("../../assets/img/food-table.jpg");
 const img2 = require("../../assets/img/grill.jpg");
-=======
-const img1 = require("../img/food-table.jpg");
-const img2 = require("../img/grill.jpg");
->>>>>>> develop
 
 $(document).ready(function() {
   // First image is hard coded in index.html
